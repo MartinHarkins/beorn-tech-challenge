13d0e8ab52ab01ef51d9c20277d2578bab01096b
Martin Harkins <martin.a.harkins@gmail.com>
2020-07-15 10:07:57 +0200

    split rhino tests from html parsing tests

--- src/test/kotlin/com/beorntech/HtmlParserTest.kt
+++ src/test/kotlin/com/beorntech/HtmlParserTest.kt
@@ -7,74 +7,8 @@ import org.mozilla.javascript.Scriptable
 
 
 class HtmlParserTest : StringSpec({
-//    "should find script tags with our server javascript handle" {
-//        val script1 = "script1"
-//        val script2 = "script2"
-//        val htmlBase = """
-//                <script type="server/javascript">$script1</script>
-//                <div/>
-//                <script type="server/javascript">$script2</script>
-//            """".trimIndent()
-//        val res = parseHtml(htmlBase)
-//        res.size shouldBe 2
-//        res[0] shouldBe script1
-//        res[1] shouldBe script2
-//    }
 
-    "should do something with a script" {
-        val script = """
-            obj = {a:1, b:['x','y']}
-        """.trimIndent()
-
-        val rhinoInterpreter = RhinoJSInterpreter();
-        rhinoInterpreter.eval(script)
-
-        val obj: Scriptable = rhinoInterpreter.getScope().get("obj", rhinoInterpreter.getScope()) as Scriptable
-        obj.get("a", obj) as Double shouldBe 1
-
-        rhinoInterpreter.close();
-    }
-
-    "should allow for pre-injected java objects in the script scope" {
-        class Dummy(name: String) {
-            var name: String = ""
-            fun writeName(name: String) {
-                this.name = name
-            }
-
-            init {
-                writeName(name)
-            }
-        }
-
-        val dummyInstance = Dummy("you")
-        val script = """
-            var initialName = dummy.name;
-            dummy.writeName("me");
-            var finalName = dummy.name;
-        """.trimIndent()
-
-        val rhinoJSInterpreter = RhinoJSInterpreter(withJavaContext = hashMapOf(Pair("dummy", dummyInstance)))
-
-        rhinoJSInterpreter.eval(script)
-
-        val scope = rhinoJSInterpreter.getScope()
-
-        val initialName: NativeJavaObject = scope.get("initialName", scope) as NativeJavaObject
-        initialName.unwrap() shouldBe "you"
-        val finalName: NativeJavaObject = scope.get("finalName", scope) as NativeJavaObject
-        finalName.unwrap() shouldBe "me"
-
-        // Go get the name in the java bean object too.
-        val dummyFromJS: NativeJavaObject = scope.get("dummy", scope) as NativeJavaObject
-        if (dummyFromJS !== Scriptable.NOT_FOUND) {
-            val theName = dummyFromJS.get("name", dummyFromJS) as NativeJavaObject
-            theName.unwrap() shouldBe "me"
-        }
-        rhinoJSInterpreter.close()
-    }
-
-    "should evaluate valid expressions" {
+    "should evaluate valid template expressions" {
         isTemplatingExpression("\${person.test}") shouldBe true
         isTemplatingExpression("\${someVar}") shouldBe true
         isTemplatingExpression("\${someFunc()}") shouldBe true
@@ -86,12 +20,6 @@ class HtmlParserTest : StringSpec({
         // sample of incomplete tests
 //        shouldEvaluate("\${()}") shouldBe false
 //        shouldEvaluate("\${2var}") shouldBe false
-    }
-
-    "should read simple expression" {
-        RhinoJSInterpreter { jsInterpreter ->
-            jsInterpreter.evalAsString(stripTemplating("\${5}")) shouldBe "5"
-        }
     }
 
     "should translate expressions within attributes and elements" {
---
+++ src/test/kotlin/com/beorntech/RhinoJSInterpreterTest.kt
@@ -0,0 +1,67 @@
+package com.beorntech
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.mozilla.javascript.NativeJavaObject
+import org.mozilla.javascript.Scriptable
+
+class RhinoJSInterpreterTest : StringSpec({
+    "should read simple expression" {
+        RhinoJSInterpreter { jsInterpreter ->
+            jsInterpreter.evalAsString(stripTemplating("\${5}")) shouldBe "5"
+        }
+    }
+
+    "should do something with a script" {
+        val script = """
+            obj = {a:1, b:['x','y']}
+        """.trimIndent()
+
+        val rhinoInterpreter = RhinoJSInterpreter();
+        rhinoInterpreter.eval(script)
+
+        val obj: Scriptable = rhinoInterpreter.getScope().get("obj", rhinoInterpreter.getScope()) as Scriptable
+        obj.get("a", obj) as Double shouldBe 1
+
+        rhinoInterpreter.close();
+    }
+
+    "should allow for pre-injected java objects in the script scope" {
+        class Dummy(name: String) {
+            var name: String = ""
+            fun writeName(name: String) {
+                this.name = name
+            }
+
+            init {
+                writeName(name)
+            }
+        }
+
+        val dummyInstance = Dummy("you")
+        val script = """
+            var initialName = dummy.name;
+            dummy.writeName("me");
+            var finalName = dummy.name;
+        """.trimIndent()
+
+        val rhinoJSInterpreter = RhinoJSInterpreter(withJavaContext = hashMapOf(Pair("dummy", dummyInstance)))
+
+        rhinoJSInterpreter.eval(script)
+
+        val scope = rhinoJSInterpreter.getScope()
+
+        val initialName: NativeJavaObject = scope.get("initialName", scope) as NativeJavaObject
+        initialName.unwrap() shouldBe "you"
+        val finalName: NativeJavaObject = scope.get("finalName", scope) as NativeJavaObject
+        finalName.unwrap() shouldBe "me"
+
+        // Go get the name in the java bean object too.
+        val dummyFromJS: NativeJavaObject = scope.get("dummy", scope) as NativeJavaObject
+        if (dummyFromJS !== Scriptable.NOT_FOUND) {
+            val theName = dummyFromJS.get("name", dummyFromJS) as NativeJavaObject
+            theName.unwrap() shouldBe "me"
+        }
+        rhinoJSInterpreter.close()
+    }
+})
